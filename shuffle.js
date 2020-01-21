c0c66dacead5226bfc6cd56054f80c42ea2a5019
SteveJustSteve <schachweinbuch@gmail.com>
2020-01-21 07:07:04 +0100

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -6,7 +6,10 @@ for (j=0; j<6; j++) {
 	arr = shuffle(arr);
 	draw = arr.slice(0,6);
 	draw.sort();
+document.write("<br>");
+document.write("Unsorted:");	
 	for (i=0; i<6; i++) { document.writeln("["+arr[i]+"]"+" ")}
+document.write("Sorted:");	
 	for (i=0; i<6; i++) { document.writeln("["+draw[i]+"]"+" ")}
 document.write("<br>");
 }
